9a3e5e7b3f7cb0d62007c49504d78ea592d912f5
Owin Ekanayake <owinlakshitha@gmail.com>
2024-09-02 09:58:14 +0530

    dependencis added

--- src/main/java/com/example/EazyBank/EazyBankApplication.java
+++ src/main/java/com/example/EazyBank/EazyBankApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EazyBankApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EazyBankApplication.class, args);
 	}
-
 }
