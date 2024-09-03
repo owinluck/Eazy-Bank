b32d2da62228dc6dc804643917f7f8b45ddb7fcc
Owin Ekanayake <owinlakshitha@gmail.com>
2024-09-03 16:36:13 +0530

    Configuring PasswordEncoder using PasswordEncoderFactories

--- src/main/java/com/example/EazyBank/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/EazyBank/config/ProjectSecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,7 +36,14 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}1234").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}4321").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$0.FbTHnUz93ggsrmIprBku.VJ6euxzK0L8aBnT5x5VRjbDM2.q5me")
+                .authorities("admin").build();
         return  new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
