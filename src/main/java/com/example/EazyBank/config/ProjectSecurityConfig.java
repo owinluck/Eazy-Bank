716da0aa4db433b40670f0d88af66e44baec3519
Owin Ekanayake <owinlakshitha@gmail.com>
2024-09-06 14:08:37 +0530

    User using a database

--- src/main/java/com/example/EazyBank/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/EazyBank/config/ProjectSecurityConfig.java
@@ -1,22 +1,17 @@
 package com.example.EazyBank.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -36,14 +31,15 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$DYLniVPrAfVpTTgvvUjwM.zdcDv.vmRYMX/mHU8Nmq7DoUFw6L6X6")
-                .authorities("admin").build();
-        return  new InMemoryUserDetailsManager(user,admin);
-    }
+    public UserDetailsService userDetailsService(DataSource dataSource){
+//        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+//        UserDetails admin = User.withUsername("admin")
+//                .password("{bcrypt}$2a$12$DYLniVPrAfVpTTgvvUjwM.zdcDv.vmRYMX/mHU8Nmq7DoUFw6L6X6")
+//                .authorities("admin").build();
+//        return  new InMemoryUserDetailsManager(user,admin);
 
+        return new JdbcUserDetailsManager(dataSource);
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
