2941cfa348bf3ac44687ce93986f576dc61c0ab6
Owin Ekanayake <owinlakshitha@gmail.com>
2024-09-11 23:26:30 +0530

    Creating our own custom implementation of UserDetailsService

--- src/main/java/com/example/EazyBank/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/EazyBank/config/ProjectSecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,16 +26,16 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-//        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
-//        UserDetails admin = User.withUsername("admin")
-//                .password("{bcrypt}$2a$12$DYLniVPrAfVpTTgvvUjwM.zdcDv.vmRYMX/mHU8Nmq7DoUFw6L6X6")
-//                .authorities("admin").build();
-//        return  new InMemoryUserDetailsManager(user,admin);
-
-        return new JdbcUserDetailsManager(dataSource);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+////        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+////        UserDetails admin = User.withUsername("admin")
+////                .password("{bcrypt}$2a$12$DYLniVPrAfVpTTgvvUjwM.zdcDv.vmRYMX/mHU8Nmq7DoUFw6L6X6")
+////                .authorities("admin").build();
+////        return  new InMemoryUserDetailsManager(user,admin);
+//
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
